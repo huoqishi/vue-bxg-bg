58b976e54ae670ffe492dfae8b386939e29b4a6a
huoqishi <wangchaohua110@xx.com>
2017-11-01 13:48:15 +0800

    d

---
+++ controllers/UserController.js
@@ -0,0 +1,22 @@
+const path = require('path')
+const express = require('express')
+const multer = require('multer')
+const Teacher = require('../models/Teacher.js')
+const upload = multer({ dest: path.resolve('../uploads') })
+const router = express.Router()
+module.exports = router
+/**
+ * @api {get} /user/status 查看用户登陆状态
+ * @apiName /user/status
+ * @apiGroup Sign
+ *
+ * @apiSuccess {string} errcode 错误标识码, 为0时表示没有错误1
+ * @apiSuccess {string} errmsg  错误的提示信息
+ * @apiSuccess {Object} data  将要响应给客户端的数据
+ */
+router.get('/user/status', (request, response) => {
+  response.send({
+    errcode: 0,
+    errmsg: '用户已登陆!'
+  })
+})
