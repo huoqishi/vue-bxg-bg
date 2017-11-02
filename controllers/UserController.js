0ac4ea1ff043188286e7701c0f77a50c292ceb26
huoqishi <wangchaohua110@foxmail.com>
2017-11-03 02:00:00 +0800

    test

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -1,7 +1,7 @@
 const path = require('path')
 const express = require('express')
 const multer = require('multer')
-const config = require('../config/config-prod.js')
+const config = require('../config/config.js')
 const Teacher = require('../models/Teacher.js')
 const upload = multer({ dest: path.resolve('../uploads') })
 const router = express.Router()
